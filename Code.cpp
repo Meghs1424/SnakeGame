c4943a4f211128f68543f63cdfff1ecda02fe15c
Megha Singh <meghasinghdelhi011@gmail.com>
2020-07-25 15:10:52 +0530

    tail added, symbols altered

--- Code.cpp
+++ Code.cpp
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ncurses.h>
 using namespace std;
 
 const int Height = 20;
@@ -9,6 +8,8 @@ bool GameOver;
 int x,y,fx,fy,score;
 enum direction {STOP, LEFT, RIGHT, UP, DOWN};
 direction dir;
+int tx[100],ty[100],nt=0;
+char f='0';
 
 void setup()
 {
@@ -31,16 +32,27 @@ void draw()
   for(int i=1;i<Height-1;i++){
     for(int j=0;j<Width;j++){
       //print snake head
-      if(i==x && j==y)
-        printf("O");
-      else if(i==fx && j==fy)
-        printf("@");
-      else{
-        if(j==0 || j==Width-1)
-          printf("#");
-        else
-          printf(" ");
+      if(j==0 || j==Width-1){
+        printf("#");
+        continue;
       }
+      if(i==x && j==y){
+        printf("%c",f);
+        continue;
+      }
+      else if(i==fx && j==fy){
+        printf("$");
+        continue;
+      }
+      int check=0;
+      for(int k=0;k<nt;k++){
+        if(tx[k]==i && ty[k]==j){
+          printf("o");
+          check=1;
+        }
+      }
+      if(check==0)
+        printf(" ");
     }
     printf("\n");
   }
@@ -80,24 +92,52 @@ void input()
 
 void logic()
 {
+  int oldx,oldy,temp;
+  if(nt>1){
+    oldx=tx[0];
+    oldy=ty[0];
+    tx[0]=x;
+    ty[0]=y;
+    for(int i=1;i<nt;i++){
+      temp=tx[i];
+      tx[i]=oldx;
+      oldx=temp;
+      temp=ty[i];
+      ty[i]=oldy;
+      oldy=temp;
+    }
+  }
+  else if(nt==1){
+    tx[0]=x;
+    ty[0]=y;
+  }
   switch(dir){
-    case LEFT:
+    case LEFT:{
       y--;
+      //f='<';
       break;
-    case RIGHT:
+    }
+    case RIGHT:{
       y++;
+      //f='>';
       break;
-    case UP:
+    }
+    case UP:{
       x--;
+      //f='^';
       break;
-    case DOWN:
+    }
+    case DOWN:{
       x++;
+      //f='*';
       break;
+    }
     case STOP:
       break;
   }
   if(x==fx && y==fy){
     score+=10;
+    nt++;
     fx = rand() % Height;
     if(fx<=0 || fx>=Height-1)
       fx=Height/2;
@@ -107,6 +147,10 @@ void logic()
   }
   if(x==0 || x==Height-1 || y==0 || y==Width-1)
     GameOver=true;
+  for(int i=0;i<nt;i++){
+    if(tx[i]==x && ty[i]==y)
+      GameOver=true;
+  }
 }
 
 int main()
